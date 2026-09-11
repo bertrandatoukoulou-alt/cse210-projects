dc9b99fe7031a9a2e6e79ee4a7ef11fe9943e926
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-09-11 17:42:53 +0000

    A try

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your name?");
+        string first_name = Console.ReadLine();
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"My name is {first_name} {last_name}!");
     }
 }
