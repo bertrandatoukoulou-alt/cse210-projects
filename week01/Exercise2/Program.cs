eba5c81b4bac1bfc7eaabc3c29bd7f064c9cd172
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-09-14 16:19:26 +0000

    Ex 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,44 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        int grade_percentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade_percentage >= 90)
+        {
+            letter = "A";
+        }
+ 
+        else if (grade_percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade_percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade_percentage >= 60)
+        {
+            letter = "D";
+        }
+        
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade_percentage >= 70)
+            Console.WriteLine("Congratulations, You passed!");
+
+        else
+            Console.WriteLine("Good luck for next time");
     }
 }
+
