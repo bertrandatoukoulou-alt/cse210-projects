d7fba0651327c195ff33fe92e3d18abdaf122e48
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-09-16 13:21:27 +0000

    exo 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.WriteLine("What is the magic number?");
+
+
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magic_number)
+
+        
+        guess = int.Parse(Console.ReadLine());
+
+        if (magic_number < guess)
+        {
+            Console.WriteLine("Go Higher");
+        }
+
+        else if (magic_number > guess)
+        {
+            Console.WriteLine("Go lower");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
