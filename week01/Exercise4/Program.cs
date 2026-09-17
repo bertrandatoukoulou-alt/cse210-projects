47426b62a32d209f88149caeade8d69fb7dfe8a9
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-09-17 17:47:39 +0000

    Ex 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        // Please note we could use a do-while loop here instead
+        int user_Number = -1;
+        while (user_Number != 0)
+        {
+            Console.Write("Enter a number. Enter 0 to stop ");
+
+            string userResponse = Console.ReadLine();
+            user_Number = int.Parse(userResponse);
+
+            // Only add the number to the list if it is not 0
+            if (user_Number != 0)
+            {
+                numbers.Add(user_Number);
+            }
+        }
+
+        // Part 1: Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+      
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
