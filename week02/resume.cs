cac159eebd73721a88ae8c7a866300ad36e2cc93
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-09-18 18:23:53 +0000

    Week 2 abstraction

---
+++ week02/resume.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class resume
+
+{
+    public string _name;
+    public string _jobs;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_name} {_jobs}");
+    }
+}
