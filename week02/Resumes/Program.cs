74142a0d80926c96f46517fc82de8f9f5138a2c2
Bertrand Atoukoulou <katoukoulou@byupathway.edu>
2026-09-18 18:38:44 +0000

    Week 2 abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 class Job
 {
-    public string _company;
-    public string _jobTitle;
-    public int _startYear;
-    public int _endYear;
+    public string JobTitle;
+    public string Company;
+    public int StartYear;
+    public int EndYear;
 
     public void Display()
     {
-        Console.WriteLine($"{_company} ({_jobTitle}) {_startYear}-{_endYear}");
+        Console.WriteLine($"{JobTitle} at {Company} ({StartYear} {EndYear})");
     }
 }
 
 class Resume
 {
-    public string _name;
-    public List<Job> _jobs = new List<Job>();
+    public string Name;
+    public List<Job> Jobs = new List<Job>();
 
     public void Display()
     {
-        Console.WriteLine($"Name: {_name}");
-
-        foreach (Job job in _jobs)
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in Jobs)
         {
             job.Display();
         }
@@ -34,23 +34,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        Job job1 = new Job
+        {
+            JobTitle = "Software Engineer",
+            Company = "Microsoft",
+            StartYear = 2019,
+            EndYear = 2022
+        };
 
-        Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        Job job2 = new Job
+        {
+            JobTitle = "Manager",
+            Company = "Apple",
+            StartYear = 2022,
+            EndYear = 2023
+        };
 
-        Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        Resume myResume = new Resume
+        {
+            Name = "Allison Rose"
+        };
 
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+        myResume.Jobs.Add(job1);
+        myResume.Jobs.Add(job2);
 
         myResume.Display();
     }
